15ca6b7ce8abd1169d93c2f09e5d476509266a83
SardorbekDevelop <111041128+SardorbekDevelop@users.noreply.github.com>
2022-09-09 09:25:48 +0500

    navbar

---
+++ GitHub.js
@@ -0,0 +1,12 @@
+// Git hub bu online store yani hamma koraoladigan va foydalanaoladigan online store
+
+// Branch bu katta store yani hamma kurish imkoniyatga ega store unda hamma yuklab oladi 
+// Branch bu filialga oxshagan  narsa branchlarda 
+// branchdan branchga otish uchun git checkout master 
+
+
+// 1 git add . bu osha papkani qolga olish 
+// 2 git commit -m 'darslik'
+// git push origin master yani branchni nomi yoziladi
+
+//
